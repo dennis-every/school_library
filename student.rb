2d9ce920b80958ce4eea117d7278f0222c300e73
Dennis Every <dennis@ansyra.com>
2023-04-12 15:55:25 -0400

    Add classroom class and association with student class

--- student.rb
+++ student.rb
@@ -1,9 +1,12 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.students << self
   end
 
   def play_hooky
