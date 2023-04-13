b430f9b067c0c1416b127a91986a54c7a712e14a
Dennis Every <dennis@ansyra.com>
2023-04-13 10:44:42 -0400

    Add methods add_student add_teacher to App

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
-require './book'
-require './person'
-
 class App
   attr_reader :books, :people
 
   def initialize
     @books = []
     @people = []
+  end
+
+  def add_teacher(teacher)
+    @people.push(teacher)
+  end
+
+  def add_student(student)
+    @people.push(student)
   end
 end
