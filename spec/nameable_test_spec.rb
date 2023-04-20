efbb66647686c6e24ffca534beb4b990e4dc9da3
Dennis Every <dennis@ansyra.com>
2023-04-20 11:06:58 -0400

    Add nameable test, and other test files

---
+++ spec/nameable_test_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require './nameable'
+
+describe Nameable do
+  it "expect to show NotImplementedError" do
+    n = Nameable.new
+    expect {n.correct_name}.to raise_error(NotImplementedError)
+  end
+end
---
+++ spec/trimmer_decorator_test_spec.rb
@@ -0,0 +1,16 @@
+require './trimmer_decorator.rb'
+
+# describe TrimmerDecorator do
+#   context "valid input" do
+#     it "expect to trim a name to 10 characters" do
+#       td = TrimmerDecorator.new
+#       name = "12345678910"
+#       expect(to.correct_name(name).length).to be "10"
+#     end
+#   end
+  
+  # context "invalid input" do
+  #   it "expect not to trim a name of 3 characters" do
+  #   end
+  # end
+# end
